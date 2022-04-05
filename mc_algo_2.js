f0e47b79eadddf6a7011e9d27f3851b099265f44
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-04-05 14:55:46 +0100

    inverse working

--- mc_algo_2.js
+++ mc_algo_2.js
@@ -5,19 +5,21 @@ input : T=[2,3,4,15,16,17,0,0,-4,5,6,7,8,9,10,1,2]
 output : [2,3,4,15,16,17,0,0,10,9,8,7,6,5,-4,1,2]    
 */
 function inverse(arr,i,j){
-    var fin;
-    if(j%2==0){
-        fin = j;
-    }else{
-        
+    var fin = (j+i)/2;
+    var temp ;
+    for(var deb=i;deb<fin;deb++){
+        temp = arr[deb];
+        arr[deb]=arr[j-(deb-i)];
+        arr[j-(deb-i)]=temp;
     }
-    for(var deb=i;deb<fin/2;deb++)
+    
 }
 
 function global(arr){
     let cmpt = 0 ; // this is a variable used to count 
     arrLengthCrescentSequence = [] ; // this array is used to store the lentgh of the different crescent sequence found in the initial array 
     arrIndexCrescentSequence = [] ; // this array is used to store the beginning index of the crescent sequence in the initial array
+    let iMaxDeb , iMaxFin;   // beginning and last index of the max sequence
     for(var i = 0 ; i< arr.length;i++){
         if(arr[i]<arr[i+1]){
             if(cmpt == 0 ){
@@ -29,7 +31,10 @@ function global(arr){
             cmpt = 0 ; // the count is to 0 to begin a new counting 
         }
     }
-
+    iMaxDeb=arrLengthCrescentSequence.indexOf(Math.max(...arrLengthCrescentSequence)); // we get the index of the biggest length of the sequences
+    iMaxFin=arrLengthCrescentSequence.indexOf(Math.max(...arrLengthCrescentSequence))+1;
+    inverse(arr,arrIndexCrescentSequence[iMaxDeb],arrIndexCrescentSequence[iMaxFin]-1);
     return arr ; 
 }
 
+//console.log(global([2,3,4,15,16,17,0,0,-4,5,6,7,8,9,10,1,2]));
