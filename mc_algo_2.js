db4c7fa045cb6d330ff4b27b68d3a66176c6066a
Yacineos <96418831+Yacineos@users.noreply.github.com>
2022-04-05 13:57:55 +0100

    I started to figure it out

---
+++ mc_algo_2.js
@@ -0,0 +1,35 @@
+/*Develop an algorithm which inverses the longest crescent sequence in unsorted array 
+of integers (ecrire un algorithme qui inverse la plus longue sequence croissante dans
+un tableau non trié);
+input : T=[2,3,4,15,16,17,0,0,-4,5,6,7,8,9,10,1,2]
+output : [2,3,4,15,16,17,0,0,10,9,8,7,6,5,-4,1,2]    
+*/
+function inverse(arr,i,j){
+    var fin;
+    if(j%2==0){
+        fin = j;
+    }else{
+        
+    }
+    for(var deb=i;deb<fin/2;deb++)
+}
+
+function global(arr){
+    let cmpt = 0 ; // this is a variable used to count 
+    arrLengthCrescentSequence = [] ; // this array is used to store the lentgh of the different crescent sequence found in the initial array 
+    arrIndexCrescentSequence = [] ; // this array is used to store the beginning index of the crescent sequence in the initial array
+    for(var i = 0 ; i< arr.length;i++){
+        if(arr[i]<arr[i+1]){
+            if(cmpt == 0 ){
+                arrIndexCrescentSequence.push(i); // we store the beginning index of the sequence in the array
+            }
+            cmpt++; // we increment because we are still on an crescent sequence 
+        }else{
+            arrLengthCrescentSequence.push(cmpt); // we store the length of the sequence 
+            cmpt = 0 ; // the count is to 0 to begin a new counting 
+        }
+    }
+
+    return arr ; 
+}
+
